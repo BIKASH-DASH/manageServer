4097ee63f4442064e533b753eb2a5594a7060e89
bikash dash <tobikashdash@gmail.com>
2022-01-25 16:42:53 +0530

    nedb database access

--- src/Authentication/UserCredentialsDBAccess.ts
+++ src/Authentication/UserCredentialsDBAccess.ts
@@ -1,11 +1,27 @@
 import { UserCredentials } from '../shared/Model';
+import * as Nedb from 'nedb';
 export class  UserCredentialsDBAccess {
+
+    private nedb: Nedb ;
+
+    constructor(){
+        this.nedb = new Nedb('database/UserCredentials.db');
+        this.nedb.loadDatabase();
+    }
     
     public async putUserCredentials(userCredentials:UserCredentials):Promise<any>{
-
+        return new Promise((resolve, reject) => {
+            this.nedb.insert(userCredentials, (err, docs) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(docs)
+                }
+            })
+        });
     }
 
     public async getUserCredentials(username:string,password:string):Promise<UserCredentials | undefined>{
-
+            throw "";
     }
 }
